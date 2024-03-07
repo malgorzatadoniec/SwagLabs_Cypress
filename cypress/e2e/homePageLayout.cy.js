61bb1abf44de82af052f382f89cc967da1a7cb13
malgorzata <malgorzata.doniec@cgi.com>
2024-03-07 12:03:39 +0100

    updates

--- cypress/e2e/homePageLayout.cy.js
+++ cypress/e2e/homePageLayout.cy.js
@@ -1,6 +1,6 @@
 
 import {homePageElements} from "../POM/homePage"
-import {homePageLayoutMethods} from "../methods/homePageLayoutMethods"
+import {ProductMethod} from "../methods/specificMetchods/ProductMethod"
 
  describe('Verify Home page layout', () => {
 
@@ -39,65 +39,13 @@ it('Verify navigation bar elements', () => {
     homePageElements.sortContainer().should('be.visible')
 })
 
-it('Verify elements of first product container', () => {
-    homePageLayoutMethods.productContainer
-    (1, 
-    4,
-    './img/sauce-backpack-1200x1500.jpg',
-    'Sauce Labs Backpack',
-    'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
-    '$29.99')
-})
+const prodId = [1, 2, 3, 4, 5, 6]
 
-it('Verify elements of second product container', () => {
-    homePageLayoutMethods.productContainer
-    (2, 
-    0,
-    './img/bike-light-1200x1500.jpg',
-    'Sauce Labs Bike Light',
-    "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
-    '$9.99')
-})
-
-it('Verify elements of third product container', () => {
-    homePageLayoutMethods.productContainer
-    (3, 
-    1,
-    './img/bolt-shirt-1200x1500.jpg',
-    'Sauce Labs Bolt T-Shirt',
-    "Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.",
-    '$15.99')
-})
-
-it('Verify elements of fourth product container', () => {
-    homePageLayoutMethods.productContainer
-    (4, 
-    5,
-    './img/sauce-pullover-1200x1500.jpg',
-    'Sauce Labs Fleece Jacket',
-    "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.",
-    '$49.99')
-})
-
-it('Verify elements of fifth product container', () => {
-    homePageLayoutMethods.productContainer
-    (5, 
-    2,
-    './img/red-onesie-1200x1500.jpg',
-    'Sauce Labs Onesie',
-    "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.",
-    '$7.99')
-})
-
-it('Verify elements of sixth product container', () => {
-    homePageLayoutMethods.productContainer
-    (6, 
-    3,
-    './img/red-tatt-1200x1500.jpg',
-    'Test.allTheThings() T-Shirt (Red)',
-    "This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.",
-    '$15.99')
-})
+prodId.forEach(prodId => {
+    it(`Verify elements of product container number ${prodId}`, () => {
+        new ProductMethod(prodId).productContainer()
+    })
+});
 
 it('Verify elements of footer', () => {
     homePageElements.footerImage()
@@ -109,5 +57,5 @@ it('Verify elements of footer', () => {
         homePageElements.smIconsTwitter().should('be.visible')
         homePageElements.smIconsFacebook().should('be.visible')
         homePageElements.smIconsLinkedin().should('be.visible')
-})
+    })
 })
